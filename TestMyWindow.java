305989ca972dcbde752a04a2325487a5c194683e
vanlanh <lanhtran175@gmail.com>
2017-03-22 22:40:09 +0700

    Bài 14: Test

---
+++ TestMyWindow.java
@@ -0,0 +1,13 @@
+package hueic.edu.vn.test;
+
+import hueic.edu.vn.ui.MyWindow;
+
+public class TestMyWindow {
+
+	public static void main(String[] args) {
+		MyWindow cuaso=new MyWindow("Học tạo cửa sổ!");
+		
+		cuaso.showWindow();
+	}
+
+}
