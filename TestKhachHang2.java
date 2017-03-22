2e87799d7e0e3ca40159c1660134fbd30e099120
vanlanh <lanhtran175@gmail.com>
2017-03-22 22:37:32 +0700

    Bài 12: Test

--- TestKhachHang2.java
+++ TestKhachHang2.java
@@ -2,22 +2,17 @@ package hueic.edu.vn.test;
 
 import java.util.ArrayList;
 
-import hueic.edu.vn.io.TextFileFactory;
+import hueic.edu.vn.io.SerializeFileFactory;
 import hueic.edu.vn.model.KhachHang;
 
-public class TestKhachHang {
-
+public class TestKhachHang2 {
 	public static void testLuuFile(){
 		ArrayList<KhachHang>dsKH=new ArrayList<KhachHang>();
 		dsKH.add(new KhachHang("KH01", "Trần Văn Lanh"));
 		dsKH.add(new KhachHang("KH02", "Phạm Hữu Lộc"));
 		dsKH.add(new KhachHang("KH03", "Bùi Thiện"));
-		
-		//Lưu file
-		boolean kt=TextFileFactory.luuFile(dsKH, "d:\\dulieu.txt");
-		//Đối số 1 dữ liệu muốn lưu, đối số 2 là nơi lưu
-		//Kiểm tra đã lưu file được hay chưa
-		if(kt==true){
+		boolean kq=SerializeFileFactory.luuFile(dsKH, "d:\\dulieu2.txt");
+		if(kq==true){
 			System.out.println("Đã lưu file thành công");
 		}else{
 			System.out.println("Lưu file thất bại");
@@ -26,12 +21,11 @@ public class TestKhachHang {
 	}
 	public static void main(String[] args) {
 		testLuuFile();
-		ArrayList<KhachHang>dsKH=TextFileFactory.docFile("d:\\dulieu.txt");
+		ArrayList<KhachHang>dsKH=SerializeFileFactory.docFile("d:\\dulieu2.txt");
 		System.out.println("Danh sách khách hàng nạp vào máy tính là:");
 		System.out.println("Mã\t Tên");
 		for(KhachHang kh: dsKH){
 			System.out.println(kh);
 		}
 	}
-
 }
