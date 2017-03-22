d9e68637718024435851a17f7db3c74addc4f814
vanlanh <lanhtran175@gmail.com>
2017-03-22 22:37:06 +0700

    Bài 12: Model

--- KhachHang.java
+++ KhachHang.java
@@ -1,62 +1,32 @@
-package hueic.edu.vn;
+package hueic.edu.vn.model;
 
-public class KhachHang {
-	//Tạo thuộc tính(bắt buộc phải để private, để đảm bảo tính đóng gói)
+import java.io.Serializable;
+
+public class KhachHang implements Serializable {
 	private String ma;
 	private String ten;
-	private int tuoi;
-	private String diaChi;
-		
-	private static double x;
-	
-	//public: khả năng truy xuất bất kỳ mọi nơi đâu
-	
-	public void doiThongTinCuaX(int gtmoi){
-		x=gtmoi;
+	public String getMa() {
+		return ma;
 	}
-	public double layThongTinX(){
-		return x;
+	public void setMa(String ma) {
+		this.ma = ma;
 	}
-		
-	//Phương thức lấy (không có đối số)
-	public String getMa(){
-		return this.ma;		//this là từ khóa truy xuất nội tại trong bản thân lớp này
+	public String getTen() {
+		return ten;
 	}
-		
-	//Phương thức thay đổi thông tin cho thuộc tính
-	public void setMa(String ma){
-		this.ma=ma;
+	public void setTen(String ten) {
+		this.ten = ten;
 	}
-	public String getTen(){
-		return this.ten;
+	public KhachHang(String ma, String ten) {
+		super();
+		this.ma = ma;
+		this.ten = ten;
 	}
-	public void setTen(String ten){
-		this.ten=ten;
+	public KhachHang() {
+		super();
 	}
-	public int getTuoi(){
-		return this.tuoi;
-	}
-	public void setTuoi(int tuoi){
-		this.tuoi=tuoi;
-	}
-	public String getDiaChi(){
-		return this.diaChi;
-	}
-	public void setDiaChi(String diaChi){
-		this.diaChi=diaChi;
-	}
-	//Constructor
-	public KhachHang(){	//Constructor mặc định không có đối số
-		System.out.println("Mặc Định");
-	}
-	//Constructor đầy đủ đối số
-	public KhachHang(String ma, String ten, int tuoi, String diaChi){
-		this.ma=ma;
-		this.ten=ten;
-		this.tuoi=tuoi;
-		this.diaChi=diaChi;
-	}
+	@Override
 	public String toString() {
-		return this.ma+";"+this.ten+";"+this.tuoi+";"+this.diaChi;
+		return this.ma+"\t"+this.ten;
 	}
 }
