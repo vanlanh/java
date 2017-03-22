504ded7ce2a7b24b558f2cdeaaf283f2b4c2bb5c
vanlanh <lanhtran175@gmail.com>
2017-03-22 22:38:21 +0700

    Bài 13: QLKH/IO

--- SerializeFileFactory.java
+++ SerializeFileFactory.java
@@ -9,14 +9,11 @@ import java.util.ArrayList;
 import hueic.edu.vn.model.KhachHang;
 
 public class SerializeFileFactory {
-	//Lưu file theo đối tượng
 	public static boolean luuFile(ArrayList<KhachHang>dsKH, String path){
 		try{
 			FileOutputStream fos=new FileOutputStream(path);
 			ObjectOutputStream oos=new ObjectOutputStream(fos);
-			//Cho phép ghi hình nguyên đối tượng xuống ổ cứng
 			oos.writeObject(dsKH);	//map nguyên đối tượng xuống ổ cứng
-			//Đóng file
 			oos.close();
 			fos.close();
 			return true;
@@ -25,7 +22,6 @@ public class SerializeFileFactory {
 		}
 		return false;
 	}
-	//Đọc file theo đối tượng
 	public static ArrayList<KhachHang>docFile(String path){
 		ArrayList<KhachHang>dsKH=new ArrayList<KhachHang>();
 		try{
@@ -33,7 +29,6 @@ public class SerializeFileFactory {
 			ObjectInputStream ois=new ObjectInputStream(fis);
 			Object data=ois.readObject();
 			dsKH=(ArrayList<KhachHang>) data;
-			//Đóng file
 			ois.close();
 			fis.close();
 		}catch(Exception ex){
